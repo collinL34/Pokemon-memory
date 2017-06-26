5571ee53ee1a8b0295ef74cf5a0203664f999557
collinl34 <collinlshelby@gmail.com>
2017-06-26 13:54:15 -0700

    @pokemon var in api shuffled to insure no exact copies are next to eachother in gameboard

--- pokemonDb/app/controllers/pokemon_controller.rb
+++ pokemonDb/app/controllers/pokemon_controller.rb
@@ -1,7 +1,11 @@
 class PokemonController < ApplicationController
+
   def index
     @pokemon = Pokemon.all.shuffle()
     @pokemon = @pokemon[(0..9)]
+    pok_clone = @pokemon.shuffle()
+    @pokemon << pok_clone
+    @pokemon.shuffle()
 
     render json: @pokemon
   end
